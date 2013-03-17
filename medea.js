43054d6b696d9a7ffe2ebed8ecf0a757b2553e3b
Kevin Swiber <kswiber@gmail.com>
2013-03-17 13:04:34 -0400

    Fixing redblack tree implementation.

--- medea.js
+++ medea.js
@@ -387,6 +387,7 @@ Medea.prototype._wrapWriteFileSync = function() {
 Medea.prototype.get = function(key, cb) {
   var entry = this.keydir.find(key);
   if (entry) {
+    entry = entry.value;
     var readBuffer = new Buffer(entry.valueSize);
     var filename = this.dirname + '/' + entry.fileId + '.medea.data';
     var fd;
