5ec98ea3c81e0834e40362ba35b7a2e865bf6594
David Björklund <david.bjorklund@gmail.com>
2014-07-19 15:45:01 +0200

    add failing tests (that only happens sometimes)

--- test/medea_test.js
+++ test/medea_test.js
@@ -1,5 +1,4 @@
 var assert = require('assert');
-var crypto = require('crypto');
 var Medea = require('../');
 
 var directory = __dirname + '/tmp/medea_test';
@@ -265,7 +264,7 @@ describe('Medea', function() {
   });
 });
 
-describe('Multiple files and compact', function () {
+describe('Multiple time overwriting same key and compact', function () {
   before(function (done) {
     require('rimraf')(directory, function () {
       db = new Medea({ maxFileSize: 100 });
@@ -273,55 +272,84 @@ describe('Multiple files and compact', function () {
     });
   });
 
-  it('successfully overwriting same key', function (done) {
-    db.put('foo1', new Buffer(100), function () {
-      db.put('foo1', new Buffer(100), function () {
-        db.put('foo1', new Buffer(100), function () {
-          db.compact(function (err) {
-            if (err) return done(err)
+  for(var i = 0; i < 10; i++) {
+    it('successfully overwriting same key', function (done) {
+      var buffer1 = new Buffer(50),
+        buffer2 = new Buffer(50),
+        buffer3 = new Buffer(50);
+
+      buffer1.fill(1);
+      buffer2.fill(2);
+      buffer3.fill(3);
+
+      db.put('foo1', buffer1, function () {
+        db.put('foo1', buffer2, function () {
+          db.put('foo1', buffer3, function () {
             db.compact(function (err) {
               if (err) return done(err)
+              db.compact(function (err) {
+                if (err) return done(err)
 
-              db.get('foo1', function (err, value) {
-                assert(!!value);
-                done()
-              })
+                db.get('foo1', function (err, value) {
+                  assert.deepEqual(value, buffer3);
+                  done();
+                })
+              });
             });
           });
         });
       });
     });
+  }
+
+  after(function (done) {
+    db.close(done);
+  });
+});
+
+describe('Multiple time overwriting same key and compact', function () {
+  before(function (done) {
+    require('rimraf')(directory, function () {
+      db = new Medea({ maxFileSize: 100 });
+      db.open(directory, done)
+    });
   });
 
-  it('successfully writing different key', function (done) {
-    var buffer1 = crypto.randomBytes(100),
-      buffer2 = crypto.randomBytes(100),
-      buffer3 = crypto.randomBytes(100);
+  for(var i = 0; i < 10; i++) {
+    it('successfully writing different key', function (done) {
+      var buffer1 = new Buffer(50),
+        buffer2 = new Buffer(50),
+        buffer3 = new Buffer(50);
 
-    db.put('foo1', buffer1, function () {
-      db.put('foo2', buffer2, function () {
-        db.put('foo3', buffer3, function () {
-          db.compact(function (err) {
-            if (err) return done(err)
+      buffer1.fill(1);
+      buffer2.fill(2);
+      buffer3.fill(3);
+
+      db.put('foo1', buffer1, function () {
+        db.put('foo2', buffer2, function () {
+          db.put('foo3', buffer3, function () {
             db.compact(function (err) {
               if (err) return done(err)
-              db.get('foo1', function (err, value) {
-                assert.deepEqual(value, buffer1);
-                db.get('foo2', function (err, value) {
-                  assert.deepEqual(value, buffer2);
-                  db.get('foo3', function (err, value) {
-                    assert.deepEqual(value, buffer3);
+              db.compact(function (err) {
+                if (err) return done(err)
+                db.get('foo1', function (err, value) {
+                  assert.deepEqual(value, buffer1);
+                  db.get('foo2', function (err, value) {
+                    assert.deepEqual(value, buffer2);
+                    db.get('foo3', function (err, value) {
+                      assert.deepEqual(value, buffer3);
 
-                    done()
+                      done()
+                    });
                   });
-                });
-              })
+                })
+              });
             });
           });
         });
       });
     });
-  });
+  }
 
   after(function (done) {
     db.close(done);
