58163e6c095c2eedca26cfba3196e066f8fb9fe5
David Björklund <david.bjorklund@gmail.com>
2014-07-29 10:24:06 +0200

    remove references to *Sync-methods in compactor

--- compactor.js
+++ compactor.js
@@ -1,3 +1,4 @@
+var EventEmitter = require('events').EventEmitter;
 var fs = require('fs');
 
 var crc32 = require('buffer-crc32');
@@ -12,11 +13,14 @@ var headerOffsets = constants.headerOffsets;
 var sizes = constants.sizes;
 
 var Compactor = module.exports = function (db) {
+  EventEmitter.call(this);
   this.db = db;
   this.activeMerge = null;
   this.delKeyDir = null;
   this.bytesToBeWritten = 0;
+  this.gettingNewActiveFile = false;
 }
+require('util').inherits(Compactor, EventEmitter);
 
 /*
  * 1. Get readable files.
@@ -47,58 +51,52 @@ Compactor.prototype.compact = function (cb) {
     return cb();
   }
 
-  this.activeMerge = DataFile.createSync(this.db.dirname);
-
-  this.db.readableFiles.push(this.activeMerge)
-
-  var self = this;
-  this._compactFile(files, 0, function(err) {
+  DataFile.create(this.db.dirname, function (err, file) {
     if (err) {
-      cb(err);
-      return;
+      return cb(err);
     }
 
-    var dataFileNames = files.map(function(f) {
-      return f.filename;
-    });
+    self.activeMerge = file;
 
-    var hintFileNames = files.map(function(f) {
-      return f.filename.replace('.data', '.hint');
-    });
+    self.db.readableFiles.push(self.activeMerge)
 
-    self.db.sync(self.activeMerge, function(err) {
+    self._compactFile(files, 0, function(err) {
       if (err) {
-        if (cb) cb(err);
+        cb(err);
         return;
       }
 
-      self.db.readableFiles = self.db.readableFiles
-        .filter(function (file) {
-          return files.indexOf(file) === -1;
-        });
+      var dataFileNames = files.map(function(f) {
+        return f.filename;
+      });
 
-      self._unlink(dataFileNames.concat(hintFileNames), 0, function(err) {
+      var hintFileNames = files.map(function(f) {
+        return f.filename.replace('.data', '.hint');
+      });
+
+      self.db.sync(self.activeMerge, function(err) {
         if (err) {
           if (cb) cb(err);
           return;
         }
 
-        if (cb) cb();
+        self.db.readableFiles = self.db.readableFiles
+          .filter(function (file) {
+            return files.indexOf(file) === -1;
+          });
+
+        self._unlink(dataFileNames.concat(hintFileNames), 0, function(err) {
+          if (err) {
+            if (cb) cb(err);
+            return;
+          }
+
+          if (cb) cb();
+        });
       });
     });
   });
 }
-
-Compactor.prototype._wrapWriteFileSync = function(oldFile) {
-  var file = DataFile.createSync(this.db.dirname);
-
-  this.activeMerge = file;
-  this.db.readableFiles.push(file);
-  oldFile.closeForWritingSync();
-  this.bytesToBeWritten = 0;
-
-  return file;
-};
 
 Compactor.prototype._unlink = function(filenames, index, cb) {
   var self = this;
@@ -219,77 +217,102 @@ Compactor.prototype._outOfDate = function(keydirs, everFound, fileEntry) {
   return true;
 };
 
-Compactor.prototype._getActiveMerge = function (bytesToBeWritten) {
-  var file;
+Compactor.prototype._getActiveMerge = function (bytesToBeWritten, cb) {
+  var that = this;
 
-  if (this.bytesToBeWritten + bytesToBeWritten >= this.db.maxFileSize)
-    file = this._wrapWriteFileSync(this.activeMerge);
-  else
-    file = this.activeMerge
+  if (this.bytesToBeWritten + bytesToBeWritten < this.db.maxFileSize) {
+    this.bytesToBeWritten += bytesToBeWritten;
+    cb(null, this.activeMerge);
+  } else {
+    this.once('newActiveMergeFile', function () {
+      that._getActiveMerge(bytesToBeWritten, cb);
+    });
 
-  this.bytesToBeWritten += bytesToBeWritten;
-  return file;
+    if (!this.gettingNewActiveFile) {
+      this.gettingNewActiveFile = true;
+      that._wrapWriteFile(function () {
+        that.gettingNewActiveFile = false;
+        that.emit('newActiveMergeFile');
+      });
+    }
+  }
 }
+
+Compactor.prototype._wrapWriteFile = function(cb) {
+  var that = this;
+  var oldFile = this.activeMerge;
+
+  DataFile.create(this.db.dirname, function (err, file) {
+    if (err) {
+      return cb(err);
+    }
+
+    that.activeMerge = file;
+    that.db.readableFiles.push(file);
+    that.bytesToBeWritten = 0;
+    oldFile.closeForWriting(cb);
+  });
+};
 
 Compactor.prototype._innerMergeWrite = function(dataEntry, cb) {
   var that = this;
   var buf = dataEntry.buffer;
 
-  var file = this._getActiveMerge(buf.length);
+  this._getActiveMerge(buf.length, function (err, file) {
+    /**
+     * [crc][timestamp][keysz][valuesz][key][value]
+     */
+    var key = dataEntry.key;
+    var value = dataEntry.value;
+    var lineBuffer = dataEntry.buffer;
 
-  /**
-   * [crc][timestamp][keysz][valuesz][key][value]
-   */
-  var key = dataEntry.key;
-  var value = dataEntry.value;
-  var lineBuffer = dataEntry.buffer;
-
-  file.write(lineBuffer, function(err) {
-    if (err) {
-      if (cb) cb(err);
-      return;
-    }
-
-    var oldOffset = file.offset;
-    file.offset += lineBuffer.length;
-
-    var totalSz = key.length + value.length + sizes.header;
-
-    var hintBufs = new Buffer(sizes.timestamp + sizes.keysize + sizes.offset + sizes.totalsize + key.length)
-
-    //timestamp
-    lineBuffer.copy(hintBufs, 0, headerOffsets.timestamp, headerOffsets.timestamp + sizes.timestamp);
-    //keysize
-    lineBuffer.copy(hintBufs, sizes.timestamp, headerOffsets.keysize, headerOffsets.keysize + sizes.keysize);
-    //total size
-    hintBufs.writeUInt32BE(totalSz, sizes.timestamp + sizes.keysize);
-    //offset
-    hintBufs.writeDoubleBE(oldOffset, sizes.timestamp + sizes.keysize + sizes.totalsize);
-    //key
-    key.copy(hintBufs, sizes.timestamp + sizes.keysize + sizes.totalsize + sizes.offset);
-
-    file.writeHintFile(hintBufs, function(err) {
+    file.write(lineBuffer, function(err) {
       if (err) {
         if (cb) cb(err);
         return;
       }
-      file.hintCrc = crc32(hintBufs, file.hintCrc);
-      file.hintOffset += hintBufs.length;
 
-      var entry = new KeyDirEntry();
-      entry.fileId = file.timestamp;
-      entry.valueSize = value.length;
-      entry.valuePosition = oldOffset + sizes.header + key.length;
-      entry.timestamp = dataEntry.timestamp;
+      var oldOffset = file.offset;
+      file.offset += lineBuffer.length;
 
-      fs.fsync(file.fd, function(err) {
+      var totalSz = key.length + value.length + sizes.header;
+
+      var hintBufs = new Buffer(sizes.timestamp + sizes.keysize + sizes.offset + sizes.totalsize + key.length)
+
+      //timestamp
+      lineBuffer.copy(hintBufs, 0, headerOffsets.timestamp, headerOffsets.timestamp + sizes.timestamp);
+      //keysize
+      lineBuffer.copy(hintBufs, sizes.timestamp, headerOffsets.keysize, headerOffsets.keysize + sizes.keysize);
+      //total size
+      hintBufs.writeUInt32BE(totalSz, sizes.timestamp + sizes.keysize);
+      //offset
+      hintBufs.writeDoubleBE(oldOffset, sizes.timestamp + sizes.keysize + sizes.totalsize);
+      //key
+      key.copy(hintBufs, sizes.timestamp + sizes.keysize + sizes.totalsize + sizes.offset);
+
+      file.writeHintFile(hintBufs, function(err) {
         if (err) {
-          if (cb) return cb(err);
+          if (cb) cb(err);
+          return;
         }
+        file.hintCrc = crc32(hintBufs, file.hintCrc);
+        file.hintOffset += hintBufs.length;
 
-        that.db.keydir[key] = entry;
+        var entry = new KeyDirEntry();
+        entry.fileId = file.timestamp;
+        entry.valueSize = value.length;
+        entry.valuePosition = oldOffset + sizes.header + key.length;
+        entry.timestamp = dataEntry.timestamp;
 
-        if (cb) cb();
+        fs.fsync(file.fd, function(err) {
+          if (err) {
+            if (cb) return cb(err);
+          }
+
+          that.db.keydir[key] = entry;
+
+          if (cb) cb();
+        });
       });
     });
   });
