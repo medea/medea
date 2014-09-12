36ed1d341500d755c66878ab37e18f824aa85662
David Björklund <david.bjorklund@gmail.com>
2014-09-12 11:19:10 +0200

    DRY test

--- test/medea_open_close_test.js
+++ test/medea_open_close_test.js
@@ -4,9 +4,7 @@ var medea = require('../');
 
 var directory = __dirname + '/tmp/medea_open_close_test';
 
-describe('Medea an not opened db', function() {
-  var db = medea();
-
+var runTests = function (db) {
   it('#put() should error', function (done) {
     db.put('beep', 'boop', function (err) {
       assert(err instanceof Error);
@@ -50,6 +48,12 @@ describe('Medea an not opened db', function() {
       done();
     });
   });
+};
+
+describe('Medea an not opened db', function() {
+  var db = medea();
+
+  runTests(db);
 });
 
 describe('Medea db that failed to open', function() {
@@ -73,49 +77,7 @@ describe('Medea db that failed to open', function() {
     tmp.close(done);
   });
 
-  it('#put() should error', function (done) {
-    db.put('beep', 'boop', function (err) {
-      assert(err instanceof Error);
-      done();
-    });
-  });
-
-  it('#get() should error', function (done) {
-    db.get('beep', function (err) {
-      assert(err instanceof Error);
-      done();
-    });
-  });
-
-  it('#remove() should error', function (done) {
-    db.remove('beep', function (err) {
-      assert(err instanceof Error);
-      done();
-    });
-  });
-
-  it('#write() should error', function (done) {
-    var batch = db.createBatch();
-
-    batch.put('hello', 'world');
-
-    db.write(batch, function (err) {
-      assert(err instanceof Error);
-      done();
-    });
-  });
-
-  it('#createSnapshot() should throw', function (done) {
-    assert.throws(db.createSnapshot.bind(db), Error);
-    done();
-  });
-
-  it('#compact() should error', function (done) {
-    db.compact(function (err) {
-      assert(err instanceof Error);
-      done();
-    });
-  });
+  runTests(db);
 });
 
 describe('Medea db closed', function() {
@@ -131,47 +93,5 @@ describe('Medea db closed', function() {
     });
   });
 
-  it('#put() should error', function (done) {
-    db.put('beep', 'boop', function (err) {
-      assert(err instanceof Error);
-      done();
-    });
-  });
-
-  it('#get() should error', function (done) {
-    db.get('beep', function (err) {
-      assert(err instanceof Error);
-      done();
-    });
-  });
-
-  it('#remove() should error', function (done) {
-    db.remove('beep', function (err) {
-      assert(err instanceof Error);
-      done();
-    });
-  });
-
-  it('#write() should error', function (done) {
-    var batch = db.createBatch();
-
-    batch.put('hello', 'world');
-
-    db.write(batch, function (err) {
-      assert(err instanceof Error);
-      done();
-    });
-  });
-
-  it('#createSnapshot() should throw', function (done) {
-    assert.throws(db.createSnapshot.bind(db), Error);
-    done();
-  });
-
-  it('#compact() should error', function (done) {
-    db.compact(function (err) {
-      assert(err instanceof Error);
-      done();
-    });
-  });
+  runTests(db);
 });
