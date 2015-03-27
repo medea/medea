60ada55959be0f48270f7d371f563ebdbeca7806
Adam Magaluk <AdamMagaluK@gmail.com>
2015-03-27 11:58:19 -0400

    Hint file parser: Added failing test for getting fileId from hint file name

--- test/medea_test.js
+++ test/medea_test.js
@@ -412,4 +412,33 @@ describe('Medea#open() when there are missing hint files', function () {
     });
   });
 
+  it('should pass with leading ./ in directory name', function (done) {
+    var directory = './test/tmp/medea_test';
+    var db1 = medea({});
+    require('rimraf')(directory, function() {
+      db1.open(directory, function(err) {
+        assert(!err);
+        db1.put('hello', 'world', function(err) {
+          assert(!err);
+          db1.close(function(err) {
+            assert(!err);
+            fs.unlink(directory + '/1.medea.hint', function() {
+              var db2 = medea({});
+              db2.open(directory, function(err) {
+                assert(!err);
+                // error is thrown before executing this callback
+                db2.get('hello', function(err, val) {
+                  assert(!err);
+                  assert.equal(val.toString(), 'world');
+                  done();
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+
+
 });
